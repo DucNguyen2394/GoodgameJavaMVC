fb3f443a73981987a95584c44e01588d88fb57fe
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-06-23 18:19:48 +0700

    update

--- goodgame.sql
+++ goodgame.sql
@@ -1,4 +1,12 @@
-Create database goodgame;
+
+create table role(
+	id BIGINT auto_increment NOT NULL PRIMARY KEY,
+	name VARCHAR(255) NOT NULL,
+    createAt timestamp,
+    createBy varchar(255),
+    modifiedAt timestamp,
+    modifiedBy varchar(255)
+)
 
 create table user(
 	id BIGINT auto_increment NOT NULL PRIMARY KEY,
@@ -8,11 +16,16 @@ create table user(
     address VARCHAR(255),
     email varchar(255),
     password varchar(255) not null,
+    status int not null,
+    roleId BIGINT NOT NULL,
     createAt timestamp,
     createBy varchar(255),
     modifiedAt timestamp,
     modifiedBy varchar(255)
 )
+
+ALTER TABLE user
+ADD CONSTRAINT fk_user_role FOREIGN KEY (roleId) REFERENCES role(id);
 
 create table game(
 	id BIGINT auto_increment NOT NULL PRIMARY KEY,
@@ -21,6 +34,8 @@ create table game(
     content text(255),
     description varchar(255),
     thumbnail varchar(255),
+    platformId BIGINT NOT NULL,
+    categoryId BIGINT NOT NULL,
     createAt timestamp,
     createBy varchar(255),
     modifiedAt timestamp,
@@ -47,6 +62,7 @@ create table category(
 
 create table orders(
 	id BIGINT auto_increment NOT NULL PRIMARY KEY,
+    userId BIGINT NOT NULL,
 	createAt timestamp,
     createBy varchar(255),
     modifiedAt timestamp,
@@ -55,19 +71,22 @@ create table orders(
 
 create table orderDetail(
 	id BIGINT auto_increment NOT NULL PRIMARY KEY,
-	CONSTRAINT order_id FOREIGN KEY (id) REFERENCES orders(id),
-	CONSTRAINT game_id FOREIGN KEY (id) REFERENCES game(id)
+    gameId BIGINT NOT NULL,
+    orderId BIGINT NOT NULL
 )
 select * from game
 
-ALTER TABLE game
-ADD CONSTRAINT fk_platform FOREIGN KEY (id) REFERENCES platform(id);
+select * from orderDetail
 
 ALTER TABLE game
-ADD CONSTRAINT fk_category FOREIGN KEY (id) REFERENCES category(id);
+ADD CONSTRAINT fk_platform FOREIGN KEY (platformId) REFERENCES platform(id);
+
+ALTER TABLE game
+ADD CONSTRAINT fk_category FOREIGN KEY (categoryId) REFERENCES category(id);
 
 ALTER TABLE orders
-ADD CONSTRAINT fk_user FOREIGN KEY (id) REFERENCES user(id);
+ADD CONSTRAINT fk_user FOREIGN KEY (userId) REFERENCES user(id);
 
+ALTER TABLE orderDetail ADD CONSTRAINT fk_orders FOREIGN KEY(orderId) REFERENCES orders(id);
 
-
+ALTER TABLE orderDetail ADD CONSTRAINT fk_game FOREIGN KEY(gameId) REFERENCES game(id);

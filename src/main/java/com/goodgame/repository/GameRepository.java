743db3f38294a48487fa6ae9a5cdc760fd186112
Đức Nguyễn <ducnguyen2394@gmail.com>
2021-06-25 20:59:45 +0700

    update

--- src/main/java/com/goodgame/repository/GameRepository.java
+++
@@ -1,9 +0,0 @@
-package com.goodgame.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.goodgame.entity.GameEntity;
-
-public interface GameRepository extends JpaRepository<GameEntity, Long> {
-
-}
